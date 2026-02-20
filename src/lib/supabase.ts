23c0fbc76faa71baae79ef2a52014fc0393be22a
Paiva Wesley <wes.desp@gmail.com>
2026-02-20 03:46:34 -0300

    chore: secure supabase credentials

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vmuwemrvoriopjwppkph.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtdXdlbXJ2b3Jpb3Bqd3Bwa3BoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NjM4MzIsImV4cCI6MjA4NDUzOTgzMn0.K4IIX9xdoiVmhB5HfGahDXuhFMSLwFGfpo1WjBVftLQ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+    throw new Error('Missing Supabase environment variables');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
